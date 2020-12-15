302985e94c585f7053f859c8726a304b3219dbf1
eggdev <eggers.brendan@gmail.com>
2020-12-14 21:10:05 -0500

    feat: cleaning out some files and making prettier work

--- sidemenu/src/pages/Page.tsx
+++ sidemenu/src/pages/Page.tsx
@@ -1,12 +1,17 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
 
 const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+  const { name } = useParams<{ name: string }>();
 
   return (
     <IonPage>
@@ -25,7 +30,6 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
       </IonContent>
     </IonPage>
   );
