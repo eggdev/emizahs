1ad58b78a0d13b03be20461404f15da1defbf477
Brendan Eggers <eggers.brendan@gmail.com>
2021-05-29 14:15:23 -0400

    refactor: changing app names lerna setup

--- packages/components/index.js
+++ packages/components/index.js
@@ -2,6 +2,4 @@ import 'react-native-gesture-handler';
 import {registerRootComponent} from 'expo';
 import StorybookUI from './storybook';
 
-export {default as Button} from './src/components/Button';
-
 registerRootComponent(StorybookUI);
