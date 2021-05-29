bab424d3eadd982d37bfcfa8d94f522588647e8f
Brendan Eggers <eggers.brendan@gmail.com>
2021-05-29 10:31:35 -0400

    feat: create react native project

---
+++ packages/Emizahs/ios/Pods/Target Support Files/Pods-Emizahs/Pods-Emizahs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Emizahs : NSObject
+@end
+@implementation PodsDummy_Pods_Emizahs
+@end
---
+++ packages/Emizahs/src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button as RNButton} from 'react-native-ui-lib';
+/**
+ *  Button Component
+ */
+const Button = (props) => <RNButton {...props} />;
+
+export default Button;
